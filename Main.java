7deb5d3b615ef2d177398fa0ee790decb714df9f
Shuvo Jyoti Banerjee <shuvojyotiprl@gmail.com>
2021-05-23 16:25:21 +0530

    Learning Jenkins

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		for(int i=0 ; i<5 ;i++) {
+			System.out.println("This time Jenking is pulling out this code from a git repository "+i);
+		}
+	}
+}
